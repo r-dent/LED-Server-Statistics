1e58428c7ccace3bf5d0ed3f9203f50ed23f03b3
Roman Gille <developer@romangille.com>
2018-09-04 13:49:22 +0200

    Parse stats and visualize with LED

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 #include <WiFiManager.h>
 #include <WebSocketsClient.h>
+#include <ArduinoJson.h>
  
 #define OLED_RESET 0  // GPIO0
 Adafruit_SSD1306 OLED(OLED_RESET);
@@ -14,7 +15,7 @@ Adafruit_SSD1306 OLED(OLED_RESET);
 #define LED_DATA_PIN D4
 CRGB leds[NUM_LEDS];
 
-bool blink = 0;
+int lightness = 255 / 5;
 
 const int buttonPin = D3;
 int buttonState = 0;
@@ -49,6 +50,17 @@ void displayLog(String text) {
   OLED.display();
 }
 
+int hueForLanguage(const char* lang) {
+  if (strcmp("en", lang) == 0) { return HUE_GREEN; }
+  if (strcmp("es", lang) == 0) { return HUE_ORANGE; }
+  if (strcmp("de", lang) == 0) { return HUE_YELLOW; }
+  if (strcmp("fr", lang) == 0) { return HUE_BLUE; }
+  if (strcmp("it", lang) == 0) { return HUE_RED; }
+  if (strcmp("pt", lang) == 0) { return HUE_PINK; }
+  if (strcmp("nl", lang) == 0) { return HUE_AQUA; }
+  if (strcmp("ru", lang) == 0) { return HUE_PURPLE; }
+}
+
 // WiFi related.
 
 WiFiManager wifiManager;
@@ -64,7 +76,7 @@ void wifiSaveConfigCallback () {
 // WebSocket related.
 
 #define HEARTBEAT_INTERVAL 25000
-#define SOCKET_SERVER "192.168.1.72"
+#define SOCKET_SERVER "rapscript.net"
 #define SOCKET_PORT 8143
 
 uint64_t heartbeatTimestamp = 0;
@@ -90,12 +102,52 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
     }
     break;
 
-    case WStype_TEXT:
+    case WStype_TEXT: {
       Serial.printf("[WSc] get text: %s\n", payload);
 
+      String payloadString = String((char*)payload);
+      // Assume that a bracket indicated a JSON array.
+      int firstBracket = payloadString.indexOf("[");
+      // Found JSON content.
+      if (firstBracket > -1) {
+        // Ignore everything before the bracket.
+        String messageString = payloadString.substring(firstBracket);
+        // Parse JSON. Assume that wa have a JSON array with [messageType, content].
+        const size_t bufferSize = JSON_ARRAY_SIZE(2) + JSON_OBJECT_SIZE(8) + 80;
+        DynamicJsonBuffer jsonBuffer(bufferSize);
+        JsonArray& message = jsonBuffer.parseArray(messageString);
+
+        if (strcmp(message[0], "statistics") == 0) {
+          JsonObject& stats = message[1];
+          int ledPos = 0;
+          String summary = "";
+          // Iterate through all returned languages.
+          for (const auto& kv : stats) {
+            int userCount = kv.value;
+            if (userCount > 0) {
+              const char* lang = kv.key;
+              int hue = hueForLanguage(lang);
+              // Set LEDs to language specific color.
+              for (int i = ledPos; i < min(ledPos + userCount, NUM_LEDS); i++) {
+                leds[i] = CHSV(hue, 255, lightness);
+              }
+              ledPos += userCount;
+              summary += String(lang) + ":" + String(userCount) + " ";
+            }
+          }
+          // Set the rest of the LEDs black.
+          for (int i = ledPos; i < NUM_LEDS; i++) {
+            leds[i] = BLACK;
+            ledPos++;
+          }
+          FastLED.show();
+          displayLog(summary);
+        }
+      }
       // send message to server
       // webSocket.sendTXT("message here");
-      break;
+    }
+    break;
 
     default:
       Serial.print("Received WS message.\n");
@@ -147,15 +199,10 @@ void loop() {
 
   buttonState = digitalRead(buttonPin);
   if (currTime - lastSwitchTime > 300 && buttonState == LOW) {
-    blink = !blink;
+    lightness = (lightness + (255 / 5)) % 255;
     lastSwitchTime = currTime;
-
-    for(int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CHSV(HUE_ORANGE, 255, 255 * (blink ? 0.5 : 0));
-    }
-    FastLED.show(); 
-
-    displayLog(blink ? "Switched ON" : "Switched OFF");
+    // Re-subscribe to trigger stats again.
+    webSocket.sendTXT("42[\"subscribe stats\",{}]");
   }
 
   // WebSocket Server.
